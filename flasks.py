7d1c058e14c1f9eab18564b0e97a720c667ad97d
bigdatachobo <bigdatachobo@gmail.com>
2021-12-09 06:28:50 +0000

    master

--- flasks.py
+++ flasks.py
@@ -3,7 +3,9 @@ app = Flask(__name__) #flask 인스턴스화 시킴
 @app.route("/")
 
 def helloworld():
-    str = "Hello World! Raspberry Jang!!!"
+    str = """
+    Hello World!<br>I'm working on the Raspberry Pi 4 !!!
+    """
     return str
 
 app.run(host='0.0.0.0', port='8000') #host = ip
